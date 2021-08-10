ffcc91dda0411cbb16ad93f78d94477dc74ae90a
Raffin You <raffin@kau.kr>
2021-08-11 04:47:37 +0900

    first init, Get selenuim

---
+++ project/crawlName.py
@@ -0,0 +1,18 @@
+import pyperclip as pyperclip
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from openpyxl import load_workbook
+
+#Copy and Paste login information
+driver = webdriver.Chrome('../source/chromedriver')
+
+def copy_login(xpath, input):
+    pyperclip.copy(input)
+    driver.find_element_by_xpath(xpath).click()
+    ActionChains(driver).key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
+    time.sleep(2)
